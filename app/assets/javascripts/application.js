f35d1c524c49e3283bcb9087251f03f40d4235f2
Tom Wicks <tom@miln.co>
2021-01-28 18:12:45 +0000

    added-oxford

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,3 +8,19 @@ if (window.console && window.console.info) {
 $(document).ready(function () {
   window.GOVUKFrontend.initAll()
 })
+
+// Adds active class to meny based on page
+
+$(function() {
+  var loc = window.location.href; // returns the full URL
+  var origin   = window.location.origin;  // returns origin
+  if(loc === origin + '/v2') {
+    $('#home').addClass('govuk-header__navigation-item--active');
+  } 
+  if(/case-studies/.test(loc)) {
+    $('#case-studies').addClass('govuk-header__navigation-item--active');
+  }
+  if (/about/.test(loc)) {
+    $('#about').addClass('govuk-header__navigation-item--active');
+  }
+});
