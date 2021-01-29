ccd4818ff5ccb8577f2f36f88ffd9d8be2e63a90
Tom Wicks <tom@miln.co>
2021-01-29 18:03:15 +0000

    better focus states

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -53,9 +53,10 @@ $( document ).ready(function() {
       }
 
       var scrollDistance = $(window).scrollTop();
+      
 
     $('.page-section').each(function(i) {
-        if ($(this).position().top <= scrollDistance) {
+        if ($(this).position().top - 150 <= scrollDistance) {
             $('.navigation a.active').removeClass('active');
             $('.navigation a').eq(i).addClass('active');
         }
@@ -63,5 +64,24 @@ $( document ).ready(function() {
 
     });
 
+    
   }
 });
+
+$(document).ready(function() {
+  $('a[href*=#]').bind('click', function(e) {
+      e.preventDefault(); // prevent hard jump, the default behavior
+
+      var target = $(this).attr("href"); // Set the target as variable
+
+      // perform animated scrolling by getting top-position of target-element and set it as scroll target
+      $('html, body').stop().animate({
+          scrollTop: $(target).offset().top -150
+      }, 600, function() {
+          location.hash = target; //attach the hash (#jumptarget) to the pageurl
+      });
+
+      return false;
+  });
+});
+
